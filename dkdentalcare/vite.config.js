6c28f1ec5db39b3f087b631ed0a62b7fcbf106c8
Pilsner12 <110965806+Pilsner12@users.noreply.github.com>
2025-03-16 23:32:49 +0100

    Update Vite configuration to include additional external dependencies for improved build performance

--- dkdentalcare/vite.config.js
+++ dkdentalcare/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['react-router-dom', '@mui/material', '@mui/icons-material', '@mui/icons-material/Menu', '@mui/icons-material/Close'],
+      external: ['react-router-dom', '@mui/material', '@mui/icons-material', '@mui/icons-material/Menu', '@mui/icons-material/Close', '@mui/material/styles', 'react', 'react-dom', 'react-scripts'],
       output: {
         globals: {
           'react-router-dom': 'ReactRouterDOM',
