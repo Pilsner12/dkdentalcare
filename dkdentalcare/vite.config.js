23d19d347af5b98b3437275444e40074e6b5f204
Pilsner12 <110965806+Pilsner12@users.noreply.github.com>
2025-03-16 23:14:27 +0100

    Enhance Vite configuration to externalize MUI and set globals for improved build output

--- dkdentalcare/vite.config.js
+++ dkdentalcare/vite.config.js
@@ -4,6 +4,15 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ['react-router-dom'],
+      external: ['@mui/material'],
+      external: ['@mui/icons-material'],
+      output: {
+        globals: {
+          'react-router-dom': 'ReactRouterDOM',
+          '@mui/material': 'MaterialUI',
+          '@mui/icons-material': 'MaterialUIIcons',
+        },
+      },
     },
   },
 });
