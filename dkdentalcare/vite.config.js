745f45b6f8d67a8a7b800c7bdc6f62fac9b607b3
Pilsner12 <110965806+Pilsner12@users.noreply.github.com>
2025-03-16 23:21:38 +0100

    Update Vite configuration to externalize additional MUI icons for improved build performance

--- dkdentalcare/vite.config.js
+++ dkdentalcare/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['react-router-dom', '@mui/material', '@mui/icons-material'],
+      external: ['react-router-dom', '@mui/material', '@mui/icons-material', '@mui/icons-material/Menu', '@mui/icons-material/Close'],
       output: {
         globals: {
           'react-router-dom': 'ReactRouterDOM',
