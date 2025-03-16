da16605de43a52d799c27a76b62599c0b6ff424e
Pilsner12 <110965806+Pilsner12@users.noreply.github.com>
2025-03-16 23:59:43 +0100

    Update Vite configuration to include 'react-calendar' in external dependencies for improved build performance

--- dkdentalcare/vite.config.js
+++ dkdentalcare/vite.config.js
@@ -5,7 +5,30 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-router-dom", "@mui/material", "@mui/icons-material", '@mui/icons-material/Menu', '@mui/icons-material/Close', '@mui/material/styles', '@mui/material/Box', '@mui/material/Button', '@mui/material/AppBar', '@mui/material/Toolbar', '@mui/material/Container', '@mui/material/IconButton', '@mui/material/Drawer', '@mui/material/List', '@mui/material/ListItem', '@mui/material/ListItemText', '@mui/material/styles', '@mui/material/ThemeProvider', '@emotion/react', '@emotion/styled', '@mui/material/Paper', '@mui/material/Typography', '@mui/material/Calendar'],
+      external: [
+        "react-router-dom",
+        "@mui/material",
+        "@mui/icons-material",
+        "@mui/icons-material/Menu",
+        "@mui/icons-material/Close",
+        "@mui/material/styles",
+        "@mui/material/Box",
+        "@mui/material/Button",
+        "@mui/material/AppBar",
+        "@mui/material/Toolbar",
+        "@mui/material/Container",
+        "@mui/material/IconButton",
+        "@mui/material/Drawer",
+        "@mui/material/List",
+        "@mui/material/ListItem",
+        "@mui/material/ListItemText",
+        "@mui/material/ThemeProvider",
+        "@emotion/react",
+        "@emotion/styled",
+        "@mui/material/Paper",
+        "@mui/material/Typography",
+        "react-calendar"
+      ],
       output: {
         globals: {
           "react-router-dom": "ReactRouterDOM",
