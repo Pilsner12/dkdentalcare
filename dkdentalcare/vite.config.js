4f203e21999055bc9eba59b6c3a8e6cd5d213f97
Pilsner12 <110965806+Pilsner12@users.noreply.github.com>
2025-03-16 23:09:27 +0100

    Add react-router-dom dependency and update Vite configuration to externalize it

--- dkdentalcare/vite.config.js
+++ dkdentalcare/vite.config.js
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'; // všechny závislosti z node_modules budou ve společném chunku "vendor"
-          }
-        }
-      }
-    }
-  }
-})
+      external: ['react-router-dom'],
+    },
+  },
+});
