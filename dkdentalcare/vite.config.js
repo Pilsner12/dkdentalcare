3ff036d2342812b9de988d8a778757b9300efb87
Pilsner12 <110965806+Pilsner12@users.noreply.github.com>
2025-03-16 23:45:42 +0100

    Update Vite configuration to externalize additional MUI components for improved build performance

--- dkdentalcare/vite.config.js
+++ dkdentalcare/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-router-dom", "@mui/material", "@mui/icons-material"],
+      external: ["react-router-dom", "@mui/material", "@mui/icons-material", '@mui/icons-material/Menu', '@mui/icons-material/Close', '@mui/material/styles', '@mui/material/Box', '@mui/material/Button', '@mui/material/AppBar', '@mui/material/Toolbar', '@mui/material/Container', '@mui/material/IconButton', '@mui/material/Drawer', '@mui/material/List', '@mui/material/ListItem', '@mui/material/ListItemText', '@mui/material/styles', '@mui/material/ThemeProvider'],
       output: {
         globals: {
           "react-router-dom": "ReactRouterDOM",
