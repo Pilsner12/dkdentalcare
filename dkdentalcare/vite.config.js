e875bb5139da668b11f6ae285337c6843277bf46
Pilsner12 <110965806+Pilsner12@users.noreply.github.com>
2025-03-16 23:50:07 +0100

    Update Vite configuration to include additional MUI components for improved build performance

--- dkdentalcare/vite.config.js
+++ dkdentalcare/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-router-dom", "@mui/material", "@mui/icons-material", '@mui/icons-material/Menu', '@mui/icons-material/Close', '@mui/material/styles', '@mui/material/Box', '@mui/material/Button', '@mui/material/AppBar', '@mui/material/Toolbar', '@mui/material/Container', '@mui/material/IconButton', '@mui/material/Drawer', '@mui/material/List', '@mui/material/ListItem', '@mui/material/ListItemText', '@mui/material/styles', '@mui/material/ThemeProvider'],
+      external: ["react-router-dom", "@mui/material", "@mui/icons-material", '@mui/icons-material/Menu', '@mui/icons-material/Close', '@mui/material/styles', '@mui/material/Box', '@mui/material/Button', '@mui/material/AppBar', '@mui/material/Toolbar', '@mui/material/Container', '@mui/material/IconButton', '@mui/material/Drawer', '@mui/material/List', '@mui/material/ListItem', '@mui/material/ListItemText', '@mui/material/styles', '@mui/material/ThemeProvider', '@emotion/react', '@emotion/styled', '@mui/material/Paper', '@mui/material/Typography', '@mui/material/Calendar'],
       output: {
         globals: {
           "react-router-dom": "ReactRouterDOM",
