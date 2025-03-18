c0aa2c43251f7aa9d8cc5832052e2a5c5bbfb6f8
Pilsner12 <110965806+Pilsner12@users.noreply.github.com>
2025-03-18 19:01:16 +0100

    Refactor Vite configuration to use 'cdn' alias for vite-plugin-cdn-import

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,33 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import ViteCdnImport from 'vite-plugin-cdn-import';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import cdn from "vite-plugin-cdn-import";
 
 export default defineConfig({
   plugins: [
     react(),
-    ViteCdnImport({
+    cdn({
       modules: [
         {
-          name: '@mui/material',
-          var: 'MaterialUI',  // Globální proměnná, kterou bude knihovna používat
-          path: 'https://cdn.jsdelivr.net/npm/@mui/material@6.4.8/umd/material-ui.production.min.js',
+          name: "@mui/material",
+          var: "MaterialUI",
+          path: "https://cdn.jsdelivr.net/npm/@mui/material@6.4.8/umd/material-ui.production.min.js",
         },
         {
-          name: '@mui/icons-material',
-          var: 'MaterialUIIcons',
-          path: 'https://cdn.jsdelivr.net/npm/@mui/icons-material@6.4.8/umd/icons-material-ui.production.min.js',
+          name: "@mui/icons-material",
+          var: "MaterialUIIcons",
+          path: "https://cdn.jsdelivr.net/npm/@mui/icons-material@6.4.8/umd/icons-material-ui.production.min.js",
         },
         {
-          name: '@emotion/react',
-          var: 'EmotionReact',
-          path: 'https://cdn.jsdelivr.net/npm/@emotion/react@11.14.0/umd/emotion-react.production.min.js',
+          name: "@emotion/react",
+          var: "EmotionReact",
+          path: "https://cdn.jsdelivr.net/npm/@emotion/react@11.14.0/umd/emotion-react.production.min.js",
         },
         {
-          name: '@emotion/styled',
-          var: 'EmotionStyled',
-          path: 'https://cdn.jsdelivr.net/npm/@emotion/styled@11.14.0/umd/emotion-styled.production.min.js',
+          name: "@emotion/styled",
+          var: "EmotionStyled",
+          path: "https://cdn.jsdelivr.net/npm/@emotion/styled@11.14.0/umd/emotion-styled.production.min.js",
         },
       ],
     }),
   ],
-  build: {
-    target: 'esnext',
-  },
 });
