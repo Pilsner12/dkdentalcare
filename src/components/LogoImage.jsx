ef038dbd3b952c8c5810e31645490fd9d2722498
Pilsner12 <110965806+Pilsner12@users.noreply.github.com>
2025-03-18 20:36:14 +0100

    Remove unused logo images and refactor LogoImage component to use a static logo

--- src/components/LogoImage.jsx
+++ src/components/LogoImage.jsx
@@ -1,26 +1,25 @@
-import PropTypes from "prop-types";
 import { Box } from "@mui/material";
-import React from 'react';
+import React from "react";
+import logo from "../assets/logo.png"; // Cesta k obrázku
 
-const LogoImage = ({ src, alt }) => {
+const LogoImage = () => {
   return (
     <Box
       sx={{
         width: "150px", // Pevná šířka boxu
         height: "80px", // Pevná výška boxu
-        padding: "10px",
+        padding: "10px", // Vnitřní odsazení
         display: "flex",
         alignItems: "center", // Vertikální zarovnání
         justifyContent: "center", // Horizontální zarovnání
         margin: "10px", // Vnější odsazení mezi boxy
         backgroundColor: "rgba(245, 245, 245, 0.9)", // Pozadí boxu
-        borderRadius: "0px", // Zaoblené rohy
         boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)", // Jemný stín
       }}
     >
       <img
-        src={src}
-        alt={alt}
+        src={logo} // Zdroj obrázku
+        alt="Logo" // Alternativní text pro obrázek
         style={{
           maxWidth: "100%", // Přizpůsobení šířky obsahu boxu
           maxHeight: "100%", // Přizpůsobení výšky obsahu boxu
@@ -28,11 +27,6 @@ const LogoImage = ({ src, alt }) => {
       />
     </Box>
   );
-};
-
-LogoImage.propTypes = {
-  src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
 };
 
 export default LogoImage;
